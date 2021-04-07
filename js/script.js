7afbfb8d07b6953b0e587488f78673edd8bc0fd4
nafeeshasnat <nafis5566@gmail.com>
2021-04-07 22:18:50 +0600

    Creative Rea

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var secondarySlider = new Splide( '#secondary-slider', {
+    fixedWidth : 370,
+    height     : 400,
+    gap        : 30,
+    rewind     : true,
+    pagination : true,
+    autoplay   : true,
+    perPage     : 2,
+    breakpoints : {
+        '600': {
+            fixedWidth : 370,
+            height     : 400,
+        }
+    },
+    classes: {
+		arrows: 'splide__arrows your-class-arrows',
+		arrow : 'splide__arrow your-class-arrow',
+		prev  : 'splide__arrow--prev your-class-prev',
+		next  : 'splide__arrow--next your-class-next',
+	},
+} ).mount();
